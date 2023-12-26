1e157945a8f5b1d8e187f51876fcc34ce1ee8fbb
Michael Davis <jrmdd1496@gmail.com>
2023-12-26 11:39:18 -0600

    Created product controller with crud operations. Need to build logic.

---
+++ src/main/java/com/mikeyd/productmanagerdb/ProductController.java
@@ -0,0 +1,27 @@
+package com.mikeyd.productmanagerdb;
+
+import java.util.List;
+
+public class ProductController {
+
+    public  void addProduct() {
+
+    }
+
+    public List<Product> getAllProducts() {
+
+    }
+
+    public Product getProductByText() {
+
+    }
+
+    public Product updateProduct (String key, String value) {
+
+    }
+
+    public void deleteProduct() {
+
+    }
+
+}
